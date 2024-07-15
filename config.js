8dbc711518bc04613c6d159b4c31d3e4c01eb9d0
Osaz3 <osazeaimien57@gmail.com>
2024-07-15 17:28:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ global.caption = process.env.CAPTION || global.caption || "ᴘᴏᴡᴇʀᴇᴅ 
 
 
 global.devs = "923184474176" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "+2349014768978";
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "+2349014768978"+2349015536586";
 global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "923184474176";
 
 
